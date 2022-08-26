4342f3a09d7b8a6336dfa477f07756bb973e201d
Cleifson Araujo <cl.raujo1@gmail.com>
2022-08-26 13:47:25 -0300

    find navigation fix

--- src/components/find/Find.jsx
+++ src/components/find/Find.jsx
@@ -4,9 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
 
 const Find = () => {
   return (
